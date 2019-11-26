2ca4e0cd4e18e63a6fbedb90f57851e5465f77b9
kpaje <kelvinmembership@gmail.com>
2019-11-26 14:31:45 -0800

    init create-react-app, npm i exif, boilerplate test exif read function

---
+++ src/api/exif-js.js
@@ -0,0 +1,10 @@
+var ExifImage = require("exif").ExifImage;
+
+try {
+  new ExifImage({ image: "test.jpg" }, function(error, exifData) {
+    if (error) console.log("Error: " + error.message);
+    else console.log(exifData); // Do something with your data!
+  });
+} catch (error) {
+  console.log("Error: " + error.message);
+}
