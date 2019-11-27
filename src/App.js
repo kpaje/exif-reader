0b46ceddba068153b8140511f1a3b5904c8fd524
kpaje <kelvinmembership@gmail.com>
2019-11-26 22:52:39 -0800

    changed to exif-js for better docs, exif data is consoling in browser

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import img from "./api/test.jpg";
+import * as exifr from "exifr";
 
 function App() {
+  const exifRead = () => {
+    exifr
+      .parse(img)
+      .then(exif => {
+        let obj = Object.assign({
+          Make: exif.Make,
+          Model: exif.Model,
+          Software: exif.Software,
+          DateTimeOriginal: exif.DateTimeOriginal,
+          ModifyDate: exif.ModifyDate,
+          Latitude: exif.latitude,
+          GPSLatitudeRef: exif.GPSLatitudeRef,
+          Longtitude: exif.longitude,
+          GPSLongitudeRef: exif.GPSLongitudeRef
+        });
+        console.log(obj);
+      })
+      .catch(console.error);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <input id="filepicker" type="file" multiple></input>
+        {exifRead()}
       </header>
     </div>
   );
