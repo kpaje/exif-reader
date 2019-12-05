c82e50c215eddf47e86f10c09750c07fd2661ba3
kpcruz <kelvinmembership@gmail.com>
2019-12-05 01:37:35 -0800

    let the testing begin

---
+++ src/_tests_/Exif.test.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Exif from "../components/Exif";
+
+describe("My Test Suite", () => {
+	it("My Test Case", () => {
+		expect(true).toEqual(true);
+	});
+});
