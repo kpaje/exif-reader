e3c974ff896da962b6f14e0779f30dfe201d14b6
kpaje <kelvinmembership@gmail.com>
2019-12-05 22:51:37 -0800

    added all components for testing rendering

--- src/_tests_/components.test.js
+++ src/_tests_/components.test.js
@@ -2,34 +2,45 @@ import React from "react";
 import Exif from "../components/Exif";
 import Filepicker from "../components/Filepicker";
 import FingerprintTable from "../components/FingerprintTable";
+import Fingerprint from "../components/Fingerprint";
 import Table from "../components/Table";
 import Thumbnail from "../components/Thumbnail";
 import renderer from "react-test-renderer";
 
 describe("Component Rendering", () => {
-	it("My Test Case", () => {
-		expect(true).toEqual(true);
-	});
+  it("My Test Case", () => {
+    expect(true).toEqual(true);
+  });
 
-	it("Exif Component", () => {
-		const ExifComponent = renderer.create(<Exif />).toJSON();
-		expect(ExifComponent).toMatchSnapshot();
-	});
+  it("Exif Component", () => {
+    const ExifComponent = renderer.create(<Exif />).toJSON();
+    expect(ExifComponent).toMatchSnapshot();
+  });
 
-	it("FingerprintTable Component", () => {
-		const FingerprintTableComponent = renderer
-			.create(<FingerprintTable />)
-			.toJSON();
-		expect(FingerprintTableComponent).toMatchSnapshot();
-	});
+  it("Filepicker Component", () => {
+    const FilepickerComponent = renderer.create(<Filepicker />).toJSON();
+    expect(FilepickerComponent).toMatchSnapshot();
+  });
 
-	it("Table Component", () => {
-		const TableComponent = renderer.create(<Table />).toJSON();
-		expect(TableComponent).toMatchSnapshot();
-	});
+  it("Fingerprint Component", () => {
+    const FingerprintComponent = renderer.create(<Fingerprint />).toJSON();
+    expect(FingerprintComponent).toMatchSnapshot();
+  });
 
-	it("Thumbnail Component", () => {
-		const ThumbnailComponent = renderer.create(<Thumbnail />).toJSON();
-		expect(ThumbnailComponent).toMatchSnapshot();
-	});
+  it("FingerprintTable Component", () => {
+    const FingerprintTableComponent = renderer
+      .create(<FingerprintTable />)
+      .toJSON();
+    expect(FingerprintTableComponent).toMatchSnapshot();
+  });
+
+  it("Table Component", () => {
+    const TableComponent = renderer.create(<Table />).toJSON();
+    expect(TableComponent).toMatchSnapshot();
+  });
+
+  it("Thumbnail Component", () => {
+    const ThumbnailComponent = renderer.create(<Thumbnail />).toJSON();
+    expect(ThumbnailComponent).toMatchSnapshot();
+  });
 });
--- src/components/Table.js
+++ src/components/Table.js
@@ -1,28 +1,28 @@
 import React from "react";
 
 function Table({ tableData }) {
-	return (
-		<table style={styles.table}>
-			{!undefined || !null ? (
-				<tbody>
-					{Object.entries(tableData).map(([key, value]) => (
-						<tr key={key}>
-							<td style={styles.table}>{key}</td>
-							<td style={styles.table}>{value.toString()}</td>
-						</tr>
-					))}
-				</tbody>
-			) : null}
-		</table>
-	);
+  return (
+    <table style={styles.table}>
+      {!undefined || !null ? (
+        <tbody>
+          {Object.entries(tableData).map(([key, value]) => (
+            <tr key={key}>
+              <td style={styles.table}>{key}</td>
+              <td style={styles.table}>{value.toString()}</td>
+            </tr>
+          ))}
+        </tbody>
+      ) : null}
+    </table>
+  );
 }
 
 export default Table;
 
 const styles = {
-	table: {
-		textAlign: "left",
-		border: "1px solid",
-		borderCollapse: "collapse"
-	}
+  table: {
+    textAlign: "left",
+    border: "1px solid",
+    borderCollapse: "collapse"
+  }
 };
