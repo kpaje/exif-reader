1857fafc41178cc49f802454d33b732f1a5256fd
kpaje <kelvinmembership@gmail.com>
2019-12-07 14:02:38 -0800

    material ui init with dashboard template

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Exif from "../components/Exif";
 import Fingerprint from "../components/Fingerprint";
+import Dashboard from "../components/Dashboard";
 
 function HomePage() {
   return (
     <div>
-      <Fingerprint />
-      <Exif />
+      {/* <Fingerprint /> */}
+      {/* <Exif /> */}
+      <Dashboard />
     </div>
   );
 }
